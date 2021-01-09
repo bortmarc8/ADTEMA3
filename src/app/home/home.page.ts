9c4aec59aeae29febddd2ad5379c0e4017dcdcd5
Mark <bortmarc8@gmail.com>
2021-01-10 00:25:09 +0100

    AE7 finished

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  titulo : string = 'QuickTrade';
+  categoria : string = 'Categoría';
 
-  constructor() {}
+  constructor(private pickerCtrl: PickerController) {}
+
+  async showBasicPicker() {
+    let opts: PickerOptions = {
+      buttons: [
+        {
+          text: 'Hecho'
+        }
+      ],
+      columns: [
+        {
+          name: 'categoria',
+          options: [
+            { text: 'Tecnología', value: 'tecnologia' },
+            { text: 'Hogar', value: 'hogar' },
+            { text: 'Inmobiliaria', value: 'inmobiliaria' },
+            { text: 'Motor', value: 'motor' },
+          ]
+        }
+      ]
+    };
+    let picker = await this.pickerCtrl.create(opts);
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let col = await picker.getColumn('categoria');
+      this.categoria = col.options[col.selectedIndex].text;
+    });
+  }
+
+
 
 }
